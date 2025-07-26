419a44c2445b08607cfb9d4323152398a206125b
Joona Gynther <joona@gynther.xyz>
2025-07-26 21:16:15 +0300

    initial matcher

---
+++ matcher/main.py
@@ -0,0 +1,43 @@
+from itertools import batched
+from pathlib import Path
+
+# Order matters
+import torch  # isort: skip
+import faiss  # isort: skip
+
+from PIL import Image
+from tqdm import tqdm
+from transformers import AutoModel, AutoProcessor
+
+MODEL = "google/siglip2-so400m-patch16-naflex"
+batch_size = 16
+
+CACHE = Path(".cache")
+IMAGES = CACHE / "images"
+INDEX = CACHE / "cards.faiss"
+IDS = CACHE / "ids.txt"
+
+
+@torch.no_grad()
+def main():
+    model = AutoModel.from_pretrained(MODEL, device_map="auto")
+    processor = AutoProcessor.from_pretrained(MODEL)
+
+    index = faiss.IndexFlatIP(1152)
+
+    with IDS.open("w") as f:
+        for batch in tqdm(batched(IMAGES.glob("*.jpg"), batch_size)):
+            images = [Image.open(path) for path in batch]
+            inputs = processor(images=images, return_tensors="pt").to(model.device)
+            embeddings = model.get_image_features(**inputs).cpu().numpy()
+
+            index.add(embeddings)  # type: ignore
+
+            for path in batch:
+                f.write(f"{path.stem}\n")
+
+    faiss.write_index(index, str(INDEX))
+
+
+if __name__ == "__main__":
+    main()

20d4108032dbc5298aa4a86fa85fc5cded31340d
Joona Gynther <joona@gynther.xyz>
2025-07-28 21:20:44 +0300

    move embedding into a small server

--- matcher/main.py
+++ matcher/main.py
@@ -1,43 +1,80 @@
+import json
 from itertools import batched
 from pathlib import Path
 
-# Order matters
-import torch  # isort: skip
-import faiss  # isort: skip
-
-from PIL import Image, ImageFile
+import faiss
+import httpx
+import numpy as np
 from tqdm import tqdm
-from transformers import AutoModel, AutoProcessor
 
-ImageFile.LOAD_TRUNCATED_IMAGES = True
-
-MODEL = "google/siglip2-so400m-patch16-naflex"
-batch_size = 16
+from extractor import find_all_cards_from
+from scrycache import refresh_scryfall_cache
 
 CACHE = Path(".cache")
+CARDS = CACHE / "cards.json"
 IMAGES = CACHE / "images"
 INDEX = CACHE / "cards.faiss"
 IDS = CACHE / "ids.txt"
 
 
-@torch.no_grad()
-def main():
-    model = AutoModel.from_pretrained(MODEL, device_map="auto")
-    processor = AutoProcessor.from_pretrained(MODEL)
+def get_embeddings(paths: list[str]) -> np.typing.NDArray[np.float32]:
+    result = httpx.post(
+        "http://localhost:8000/embed",
+        json=paths,
+        timeout=60,
+    )
 
-    index = faiss.IndexFlatIP(1152)
+    result.raise_for_status()
+    json = result.json()
+
+    return np.array(json, dtype=np.float32)
+
+
+def create_index():
+    index = faiss.IndexFlatL2(768)  # 1152
 
     with IDS.open("w") as f:
-        for batch in tqdm(batched(IMAGES.glob("*.jpg"), batch_size)):
-            images = [Image.open(path) for path in batch]
-            inputs = processor(images=images, return_tensors="pt").to(model.device)
-            embeddings = model.get_image_features(**inputs).cpu().numpy()
+        for batch in tqdm(batched(IMAGES.glob("*.jpg"), 128)):
+            paths = [str(path) for path in batch]
+            embeddings = get_embeddings(paths)
             index.add(embeddings)  # type: ignore
 
             for path in batch:
                 f.write(f"{path.stem}\n")
 
     faiss.write_index(index, str(INDEX))
+
+
+def main():
+    # refresh_scryfall_cache()
+
+    if not INDEX.exists():
+        create_index()
+
+    index: faiss.IndexFlatL2 = faiss.read_index(str(INDEX))
+
+    with IDS.open() as f:
+        ids = f.read().splitlines()
+
+    with CARDS.open() as f:
+        cards = json.load(f)
+
+    images = find_all_cards_from("images/IMG_5097.jpeg")
+    embeddings = get_embeddings(images)
+    scores, indices = index.search(embeddings, k=5)  # type: ignore
+
+    for i, (scores, matches) in enumerate(zip(scores, indices)):
+        print("img:", images[i])
+        for score, match in zip(scores, matches):
+            id = ids[match]
+            for card in cards:
+                if card["id"] == id:
+                    print(score, card["scryfall_uri"])
+                    break
+
+        print()
+
+    print("Done")
 
 
 if __name__ == "__main__":
---
+++ tiny-embed-server/server.py
@@ -0,0 +1,34 @@
+from itertools import batched
+
+import torch
+from fastapi import FastAPI
+from PIL import Image, ImageFile
+from transformers import AutoModel, AutoProcessor
+
+app = FastAPI()
+
+# MODEL = "google/siglip2-so400m-patch16-naflex"
+MODEL = "facebook/dinov2-base"
+batch_size = 128
+
+model = AutoModel.from_pretrained(MODEL, device_map="auto").eval()
+processor = AutoProcessor.from_pretrained(MODEL, use_fast=True)
+
+
+@torch.no_grad()
+def get_embeddings(images: list[ImageFile.ImageFile]):
+    inputs = processor(images=images, return_tensors="pt").to(model.device)
+    outputs = model(**inputs)
+    return outputs.last_hidden_state[:, 0].cpu().tolist()
+
+
+@app.post("/embed")
+def embed(images: list[str]):
+    result = []
+
+    for batch in batched(images, batch_size):
+        imgs = [Image.open(path) for path in batch]
+        features = get_embeddings(imgs)
+        result += features
+
+    return result

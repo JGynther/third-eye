a4295b587b3c589ce4d306ded9f446b14b401282
Joona Gynther <joona@gynther.xyz>
2025-09-13 02:09:38 +0300

    use dinov3 and serve yolo

--- tiny-embed-server/server.py
+++ tiny-embed-server/server.py
@@ -5,32 +5,58 @@ from fastapi import FastAPI
 from PIL import Image, ImageFile
 from transformers import AutoModel, AutoProcessor
 
+from extractor import find_all_cards_from_obbox
+
 app = FastAPI()
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-# MODEL = "google/siglip2-so400m-patch16-naflex"
-MODEL = "facebook/dinov2-base"
-batch_size = 128
+MODEL = "facebook/dinov3-vits16-pretrain-lvd1689m"
 
-model = AutoModel.from_pretrained(MODEL, device_map="auto").eval()
 processor = AutoProcessor.from_pretrained(MODEL, use_fast=True)
+model = AutoModel.from_pretrained(
+    MODEL,
+    dtype=torch.bfloat16,
+    device_map="auto",
+    attn_implementation="sdpa",
+)
 
 
-@torch.no_grad()
+@torch.inference_mode()
 def get_embeddings(images: list[ImageFile.ImageFile]):
     inputs = processor(images=images, return_tensors="pt").to(model.device)
     outputs = model(**inputs)
-    return outputs.last_hidden_state[:, 0].cpu().tolist()
+    embeddings = outputs.pooler_output
+
+    return embeddings.float().cpu().tolist()
+
+
+def load_images(paths: tuple[str, ...]):
+    imgs = []
+
+    for path in paths:
+        img = Image.open(path)
+
+        if img.mode != "RGB":
+            img = img.convert("RGB")
+
+        imgs.append(img)
+
+    return imgs
 
 
 @app.post("/embed")
-def embed(images: list[str]):
+async def embed(images: list[str], batch_size: int = 128):
     result = []
 
     for batch in batched(images, batch_size):
-        imgs = [Image.open(path) for path in batch]
+        imgs = load_images(batch)
         features = get_embeddings(imgs)
         result += features
 
     return result
+
+
+@app.post("/bbox")
+async def bbox(img: str):
+    return find_all_cards_from_obbox(img)

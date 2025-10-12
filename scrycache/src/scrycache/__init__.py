1ae5e1d14c6cff2f012460d289850ff55598d882
Joona Gynther <joona@gynther.xyz>
2025-10-12 23:26:53 +0300

    define scrycache as an async func

--- scrycache/src/scrycache/__init__.py
+++ scrycache/src/scrycache/__init__.py
@@ -1,7 +1,5 @@
-import asyncio
-
 from .cache import download_with_cache
 
 
-def refresh_scryfall_cache():
-    asyncio.run(download_with_cache())
+async def refresh_scryfall_cache():
+    await download_with_cache()

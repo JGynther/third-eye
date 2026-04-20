aa5e90a950f14eb7d50f4f16006be88acad4f4c6
Joona Gynther <joona@gynther.xyz>
2026-04-21 02:09:08 +0300

    update hostname

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
   server: {
     host: true,
-    allowedHosts: ["nixos-gaming"],
+    allowedHosts: ["hydrogen"],
     proxy: {
       "/api": {
         target: "http://localhost:8001",
